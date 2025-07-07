406dc8e526bdc26599ea289efeb9fbbcfa18e564
João Pedro <joaopedro.oliveirads@gmail.com>
2025-07-06 22:52:34 -0300

    Fix: import corrigido de Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import DadosDaVitima from './pages/IdentficacaoPorPartes/DadosDaVitima';
 import DadosDoAgressor from './pages/IdentficacaoPorPartes/DadosDoAgressor';
 import VinculoEntrePartes from './pages/IdentficacaoPorPartes/VinculeEntrePartes';
